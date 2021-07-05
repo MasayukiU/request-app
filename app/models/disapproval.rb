d545112cd1bfa10f62b5dd6e6ade1e250cb9aefb
MasayukiU <68602249+MasayukiU@users.noreply.github.com>
2021-07-06 04:07:52 +0900

    モデル作成

---
+++ app/models/disapproval.rb
@@ -0,0 +1,2 @@
+class Disapproval < ApplicationRecord
+end
--- db/migrate/20210705185900_create_items.rb
+++ db/migrate/20210705185900_create_items.rb
@@ -1,7 +1,14 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.string  :name
+      t.integer :regular_id
+      t.string  :quantity,   null: false
+      t.string  :price,   null: false
+      t.string  :total_price,    null: false
+      t.string  :description
+      t.text :image
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
